21640fdd63425ece69f64b7c7b536451ecde8a91
syky0405work <154429021+syky0405work@users.noreply.github.com>
2024-01-17 22:18:48 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="jp">
+
+<head>
+  <meta charset="UTF-8">
+
+  <title>PHP7章課題</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    // 連想配列に値を代入
+    $price_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    // 出力
+    print_r($price_data);
+    ?>
+  </p>
+
+
+</body>
+
+</html>
